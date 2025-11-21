58f89a86fea2549c5cf81df063b533c066f6c51f
Batuhan Taluk <wtaluk2002@gmail.com>
2025-11-21 17:35:23 +0300

    Initial commit

---
+++ app/components/Video.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Video = () => {
+  return (
+    <div>
+      <video src="/314643_small.mp4"></video>
+    </div>
+  )
+}
+
+export default Video
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Video from "./components/Video";
+
+export default function Home() {
+  return (
+    <div>
+      <Video />
+    </div>
+    
+  );
+}
